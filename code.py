6bafebd4e53dd506029e877d52474d5c44bcc7ea
Sourav Mukherjee <souravjsr@yahoo.com>
2022-04-29 10:43:31 +0530

    Adding the contains-duplicate code from neetcode

---
+++ code.py
@@ -0,0 +1,10 @@
+# Ref: https://leetcode.com/problems/contains-duplicate/
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashmap = {}
+        for i in nums:
+            if i in hashmap:
+                return True
+            else:
+                hashmap[i] = 1
+        return False
